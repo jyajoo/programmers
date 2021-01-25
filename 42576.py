3713e889172ce987778e0e72b43867ab9cbca8c2
jyajoo <jyajoo1020@gmail.com>
2021-01-26 02:53:51 +0900

    완주하지 못한 선수

--- 42576.py
+++ 42576.py
@@ -1,13 +1,29 @@
+#완주하지 못한 선수
+
 from collections import Counter 
 
-def new_func():
-    p=	["marina", "josipa", "nikola", "vinko", "filipa"]
-    c= ["josipa", "filipa", "marina", "nikola"]
+def solution(p, c):
     p = Counter(p)
     c = Counter(c)
-    answer = ""
+    answer = ''
     for person, cnt in p.items():
         if p[person] != c[person]:
             answer += person
     return answer
-print(new_func())
+
+#####################################
+#완주하지 못한 선수_2
+from collections import Counter 
+
+def solution(p, c):
+    p = dict(Counter(p))
+    c = dict(Counter(c))
+    answer = ''
+    for person, cnt in p.items():
+        try:
+            if p[person] != c[person]:
+                answer += person
+        except:
+            answer += person
+    return answer
+####*다른 사람 풀이도 참고하기!
